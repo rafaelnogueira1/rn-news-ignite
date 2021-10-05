542b273f74f61a0a59bd725e9f97e3b2c803aeb7
Rafael Nogueira <rafael.nogueira@gmail.com>
2021-10-05 17:54:08 -0300

    feature: Definição de estilo global.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Home | RN. News</title>
+      </Head>
       <h1>
         Welcome to <a href='https://nextjs.org'>Next.js!</a>
       </h1>
-    </div>
+    </>
   );
 }
 
